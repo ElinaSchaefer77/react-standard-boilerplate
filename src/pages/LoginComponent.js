71a92311f63034da8337908da5c236f73b4c639e
ElinaSchaefer77 <elinaschaefer@gmail.com>
2022-01-10 11:01:25 +0100

    add login form data

---
+++ src/pages/LoginComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function HomeComponent({ onSubmit }) {
+  return (
+    <div>
+      <form obSubmit={() => onSubmit()}>
+        <input type="text" placeholder="Benutzername" />
+        <input type="password" placeholder="Passwort" />
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
